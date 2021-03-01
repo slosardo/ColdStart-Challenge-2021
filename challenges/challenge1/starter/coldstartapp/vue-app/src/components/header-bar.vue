f09a30f02d2a5c5a5d3f4e17ba641ba4e70d6c37
slosardo <silvio.losardo@gmail.com>
2021-03-01 22:48:52 +0100

    Still testing commits

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
@@ -21,7 +21,7 @@ export default {
       <HeaderBarBrand></HeaderBarBrand>
       <div class="navbar-menu">
         <div class="navbar-start">
-          <router-link class="navbar-item nav-home" to="/">Home-Login</router-link>
+          <router-link class="navbar-item nav-home" to="/">Home</router-link>
         </div>
       </div>
     </nav>
